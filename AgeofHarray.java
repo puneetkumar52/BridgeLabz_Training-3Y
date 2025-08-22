51b60dd2f0e5a7f01d186a2b6e5f4ea3ccbcb3f4
PuneetKumar <p03k10@gmail.com>
2025-08-22 22:02:19 +0530

    Initial commit

---
+++ AgeofHarray.java
@@ -0,0 +1,12 @@
+
+public class AgeofHarray {
+    public static void main(String[] args) {
+        int birthyear = 2000;
+        int currentyear = 2024;
+ 
+        int age = currentyear - birthyear;
+
+        System.out.println("the age of Harry is " + age);
+
+    }
+}
---
+++ AvgofSamMarks.java
@@ -0,0 +1,12 @@
+
+public class AvgofSamMarks {
+    public static void main(String[] args) {
+        int maths = 94;
+        int phy = 95;
+        int chem = 96;
+
+        int avg = (maths + phy + chem) / 3;
+
+        System.out.println("Sam’s average mark in PCM is " + avg);
+    }
+}
